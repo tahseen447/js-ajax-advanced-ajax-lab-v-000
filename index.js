ce8ff1bee205da9f4abc9bb68aeb23cb5c3e61ab
Tahseen Siddiqui <tahseenfatima1986@gmail.com>
2019-02-26 23:35:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function getRepositories() {
   req.open('GET', 'https://api.github.com/users/octocat/repos');
   req.send();
 }
+
 function showRepositories(event, data) {
   const repos = JSON.parse(this.responseText);
   const repoList = `<ul>${repos
